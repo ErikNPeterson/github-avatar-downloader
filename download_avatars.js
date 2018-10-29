9603669a89ffa9ba47a8582468dd9336d202f6c3
vagrant <erikeh1@gmail.com>
2018-10-29 21:56:52 +0000

    Please enter the commit message for your changes. Lines starting

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -24,6 +25,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
       cb(err, JSON.parse(body));
   });
 }
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)     
+  .on('error', function (err) {                                  
+    throw err; 
+  })
+  .on('response', function (response) {                         
+    console.log('Response Status Code: ', response.statusCode);
+    console.log(response.statusMessage) 
+    console.log('Downloading image...');
+    console.log(response.headers['content-type']); // we use bracket notation because there is a "-" in the property/key
+  })
+  .pipe(fs.createWriteStream(filePath))
+  .on('finish', function() {
+   console.log('Download complete.')
+   console.log('Response stream complete.');
+ });
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+
+
 
   // function to parse the JSON string into an object and pass this object 
   // (an array of contributor objects) to the cb function.
